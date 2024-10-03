d23539fbaf5547e33a8d634343fc20fe85d4e341
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-10-03 18:21:07 +0200

    Fix oxNewService
    use it as Integration-Test instead of Unit because oxNew is necessary for test

---
+++ tests/Integration/Core/Service/DummyClass.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidSolutionCatalysts\TeleCash\Tests\Integration\Core\Service;
+
+/**
+ * Class DummyClass
+ *
+ * This is a simple dummy class used for testing the OxNewService.
+ * It is specifically designed to be used in OxNewServiceIntegrationTest.
+ *
+ * @see OxNewServiceTest
+ */
+class DummyClass
+{
+}
