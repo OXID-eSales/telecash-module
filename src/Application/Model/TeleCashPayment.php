84012ec06a175b6df3b236d9f14029eb65ea716f
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-10-22 21:18:22 +0200

    refactor TeleCashPayment Model for better testing

--- src/Application/Model/TeleCashPayment.php
+++ src/Application/Model/TeleCashPayment.php
@@ -13,42 +13,84 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
 use OxidEsales\Eshop\Core\Model\BaseModel;
 use OxidEsales\EshopCommunity\Internal\Framework\Database\ConnectionProviderInterface;
+use OxidSolutionCatalysts\TeleCash\Exception\TeleCashException;
+use OxidSolutionCatalysts\TeleCash\Application\Model\Interface\TeleCashPaymentInterface;
 use OxidSolutionCatalysts\TeleCash\Core\Module;
+use OxidSolutionCatalysts\TeleCash\Core\Service\TeleCashPaymentValidatorService;
 use OxidSolutionCatalysts\TeleCash\Traits\DataGetter;
 use OxidSolutionCatalysts\TeleCash\Traits\ServiceContainer;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 
-class TeleCashPayment extends BaseModel
+class TeleCashPayment extends BaseModel implements TeleCashPaymentInterface
 {
     use ServiceContainer;
     use DataGetter;
 
+    protected ?string $paymentId = null;
+    protected ?string $telecashIdent = null;
+    protected ?string $captureType = null;
+
     protected Connection $connection;
 
-    /**
-     * Current class name
-     *
-     * @var string
-     */
     protected $_sClassName = 'OxidSolutionCatalysts\TeleCash\Application\TeleCashPayment';
-
-    /**
-     * Core table name
-     *
-     * @var string
-     */
     protected $_sCoreTable = Module::TELECASH_PAYMENT_EXTENSION_TABLE;
 
     /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * Constructor for TeleCashPayment.
+     *
+     * Initializes a new instance of the TeleCashPayment class. This constructor can be used
+     * in both production and test environments due to its flexible parameter configuration.
+     *
+     * @param TeleCashPaymentValidatorService $validator   The validator service to check payment existence
+     * @param Connection|null                 $connection  Optional database connection. If null, the connection
+     *                                                    will be retrieved from the service container.
+     *                                                    Primarily used for testing.
+     * @param bool                           $initParent  Whether to initialize the parent BaseModel.
+     *                                                    Set to false in test environment to avoid
+     *                                                    OXID framework dependencies. Default is true.
+     *
+     * @throws ContainerExceptionInterface     If there's an error with the service container
+     * @throws NotFoundExceptionInterface      If a required service is not found in the container
      */
-    public function __construct()
-    {
-        parent::__construct();
+    public function __construct(
+        private readonly TeleCashPaymentValidatorService $validator,
+        ?Connection $connection = null,
+        bool $initParent = true
+    ) {
+        if ($initParent) {
+            parent::__construct();
+        }
+
         $this->init($this->_sCoreTable);
-        $this->connection = $this->getServiceFromContainer(ConnectionProviderInterface::class)->get();
+        $this->connection = $connection ?? $this->getServiceFromContainer(ConnectionProviderInterface::class)->get();
+    }
+
+    /**
+     * Sets values directly for testing purposes.
+     *
+     * This method provides a way to set the internal state of the payment object
+     * without relying on the OXID framework's assign() method. It is specifically
+     * designed for testing scenarios where we want to avoid the complexity and
+     * dependencies of the framework's data handling.
+     *
+     * IMPORTANT: This method should only be used in test environments. In production code,
+     * always use the standard setters or assign() method to ensure proper data handling
+     * and validation.
+     *
+     * @param string $paymentId    The payment ID to set. This corresponds to the OXPAYMENTID field.
+     * @param string $ident        The TeleCash ident to set. Must be one of the valid idents
+     *                            defined in Module::TELECASH_PAYMENT_IDENTS.
+     * @param string $captureType  The capture type to set. Must be one of the valid types
+     *                            defined in Module::TELECASH_CAPTURE_TYPES for the given ident.
+     *
+     * @internal This method is intended for testing purposes only
+     */
+    public function setTestValues(string $paymentId, string $ident, string $captureType): void
+    {
+        $this->paymentId = $paymentId;
+        $this->telecashIdent = $ident;
+        $this->captureType = $captureType;
     }
 
     /**
@@ -73,7 +115,10 @@ class TeleCashPayment extends BaseModel
 
         try {
             $result = $this->connection->fetchAssociative($query);
-            $this->_isLoaded = is_array($result) && $this->assign($result);
+            if (is_array($result)) {
+                $this->assign($result);
+                $this->_isLoaded = true;
+            }
         } catch (Exception) {
             $this->_isLoaded = false;
         }
@@ -84,40 +129,113 @@ class TeleCashPayment extends BaseModel
     /** setter for PaymentId */
     public function setPaymentId(string $paymentId): void
     {
-        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_OXPAYMENTID => $paymentId]);
+        $this->paymentId = $paymentId;
+        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_OXPAYMENTID => $this->paymentId]);
     }
 
+    /** getter for PaymentId */
     public function getPaymentId(): string
     {
-        return $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_OXPAYMENTID);
+        if (is_null($this->paymentId)) {
+            $this->paymentId = $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_OXPAYMENTID);
+        }
+        return $this->paymentId;
     }
 
-    public function setTeleCashIdent(string $ident = ''): void
+    /** getter for possible TeleCash Idents */
+    public function getPossibleTeleCashIdents(): array
     {
-        $ident = in_array($ident, Module::TELECASH_PAYMENT_IDENTS, true) ?
-            $ident :
-            Module::TELECASH_PAYMENT_IDENT_DEFAULT;
-
-        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_IDENT => $ident]);
+        return Module::TELECASH_PAYMENT_IDENTS;
     }
 
+    /** getter for TeleCash Ident */
     public function getTeleCashIdent(): string
     {
-        return $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_IDENT);
+        if (is_null($this->telecashIdent)) {
+            $this->telecashIdent = $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_IDENT);
+        }
+        return $this->telecashIdent;
     }
 
-    public function setTeleCashCaptureType(string $captureType = ''): void
+    /** setter for TeleCash Ident */
+    public function setTeleCashIdent(string $ident = ''): void
     {
-        $captureTypes = Module::TELECASH_CAPTURE_TYPES[$this->getTeleCashIdent()];
-        $captureType = in_array($captureType, $captureTypes, true) ?
-            $captureType :
-            Module::TELECASH_CAPTURE_TYPE_DIRECT;
+        $this->telecashIdent = $this->validTeleCashIdent($ident);
 
-        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_CAPTURETYPE => $captureType]);
+        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_IDENT => $this->telecashIdent]);
     }
 
+    /** validate the TeleCash Ident */
+    public function validTeleCashIdent(string $ident = ''): string
+    {
+        return $ident && in_array($ident, $this->getPossibleTeleCashIdents(), true) ?
+            $ident :
+            Module::TELECASH_PAYMENT_IDENT_DEFAULT;
+    }
+
+    /** getter for possible TeleCash Capture-Types */
+    public function getPossibleTeleCashCaptureTypes(string $ident = ''): array
+    {
+        $ident = $this->validTeleCashIdent($ident);
+        return Module::TELECASH_CAPTURE_TYPES[$ident];
+    }
+
+    /** getter for Capture Type */
     public function getTeleCashCaptureType(): string
     {
-        return $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_CAPTURETYPE);
+        if (is_null($this->captureType)) {
+            $this->captureType = $this->getFieldStringData(Module::TELECASH_PAYMENT_EXTENSION_TABLE_CAPTURETYPE);
+        }
+        return $this->captureType;
+    }
+
+    /** setter for Capture Type */
+    public function setTeleCashCaptureType(string $captureType = ''): void
+    {
+        $this->captureType = $this->validTeleCashCaptureType($captureType);
+
+        $this->assign([Module::TELECASH_PAYMENT_EXTENSION_TABLE_CAPTURETYPE => $this->captureType]);
+    }
+
+    /** validate the TeleCash Ident */
+    public function validTeleCashCaptureType(string $captureType = '', string $ident = ''): string
+    {
+        $ident = $this->validTeleCashIdent($ident);
+        $captureTypes = $this->getPossibleTeleCashCaptureTypes($ident);
+        return $captureType && in_array($captureType, $captureTypes, true) ?
+            $captureType :
+            Module::TELECASH_CAPTURE_TYPE_DIRECT;
+    }
+
+    /**
+     * Validates if the payment exists before saving
+     */
+    public function validateBeforeSave(): bool
+    {
+        return $this->validator->checkIfPaymentExists(
+            $this->getPaymentId(),
+            $this->getTeleCashIdent(),
+            $this->getTeleCashCaptureType()
+        );
+    }
+
+    /**
+     * OXID Core
+     *
+     * {@inheritDoc}
+     *
+     * @throws TeleCashException
+     *
+     * @return string|bool
+     */
+    public function save()
+    {
+        $result = false;
+
+        if ($this->validateBeforeSave()) {
+            $result = parent::save();
+        }
+
+        return $result;
     }
 }
--- tests/Unit/Application/Model/TeleCashPaymentTest.php
+++ tests/Unit/Application/Model/TeleCashPaymentTest.php
@@ -1,94 +1,97 @@
 <?php
 
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
 namespace OxidSolutionCatalysts\TeleCash\Tests\Unit\Application\Model;
 
 use Doctrine\DBAL\Connection;
-use OxidEsales\EshopCommunity\Internal\Framework\Database\ConnectionProviderInterface;
 use OxidSolutionCatalysts\TeleCash\Application\Model\TeleCashPayment;
 use OxidSolutionCatalysts\TeleCash\Core\Module;
-use OxidSolutionCatalysts\TeleCash\Core\Service\OxNewService;
+use OxidSolutionCatalysts\TeleCash\Core\Service\TeleCashPaymentValidatorService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use ReflectionClass;
-use ReflectionException;
 
-/**
- * Class TeleCashPaymentTest
- *
- * This class contains unit tests for the TeleCashPayment model.
- */
 class TeleCashPaymentTest extends TestCase
 {
-    /** @var TeleCashPayment|MockObject The mocked TeleCashPayment object */
     private TeleCashPayment|MockObject $teleCashPayment;
-
-    /** @var Connection|MockObject The mocked database connection */
     private Connection|MockObject $connectionMock;
+    private TeleCashPaymentValidatorService|MockObject $validatorMock;
 
-    /** @var ContainerInterface|MockObject The mocked container */
-    private MockObject|ContainerInterface $containerMock;
-
-    /** @var OxNewService|MockObject The mocked OxNewService */
-    private OxNewService|MockObject $oxNewServiceMock;
-
-    /**
-     * Set up the test environment before each test
-     * @throws ReflectionException
-     */
     protected function setUp(): void
     {
         // Create mock for database connection
         $this->connectionMock = $this->createMock(Connection::class);
-        $connectionProviderMock = $this->createMock(ConnectionProviderInterface::class);
-        $connectionProviderMock->method('get')->willReturn($this->connectionMock);
 
-        // Create mock for container
-        $this->containerMock = $this->createMock(ContainerInterface::class);
-        $this->containerMock->method('get')
-            ->with(ConnectionProviderInterface::class)
-            ->willReturn($connectionProviderMock);
+        // Create mock for validator service
+        $this->validatorMock = $this->createMock(TeleCashPaymentValidatorService::class);
 
-        // Create mock for OxNewService
-        $this->oxNewServiceMock = $this->createMock(OxNewService::class);
+        // Create mock for TeleCashPayment
+        $this->teleCashPayment = $this->getMockBuilder(TeleCashPayment::class)
+            ->setConstructorArgs([
+                $this->validatorMock,
+                $this->connectionMock,
+                false
+            ])
+            ->onlyMethods([
+                'init',
+                'getPaymentId',
+                'setPaymentId',
+                'getTeleCashIdent',
+                'setTeleCashIdent',
+                'getTeleCashCaptureType',
+                'setTeleCashCaptureType',
+                'addField',
+                'assign',
+                'getViewName',
+                'getShopId',
+                'buildSelectString',
+                'isLoaded'
+            ])
+            ->getMock();
 
-        // Create a partial mock for TeleCashPayment
-        $this->teleCashPayment = $this->createPartialMock(TeleCashPayment::class, [
-            'getServiceFromContainer',
-            'getPaymentId',
-            'setPaymentId',
-            'getTeleCashIdent',
-            'setTeleCashIdent',
-            'getTeleCashCaptureType',
-            'setTeleCashCaptureType',
-        ]);
-
-        // Configure the mocked TeleCashPayment
-        $this->teleCashPayment->method('getServiceFromContainer')
-            ->willReturn($connectionProviderMock);
-
-        // Configure oxNewServiceMock to return our mocked TeleCashPayment
-        $this->oxNewServiceMock->method('oxNew')
-            ->with(TeleCashPayment::class)
-            ->willReturn($this->teleCashPayment);
-
-        // Manually set the connection
-        $this->setProperty($this->teleCashPayment, 'connection', $this->connectionMock);
+        // Mock init to prevent oxNew calls
+        $this->teleCashPayment->method('init')
+            ->willReturn(true);
     }
 
     /**
-     * Helper method to set protected/private properties
-     *
-     * @param object $object The object to modify
-     * @param string $propertyName The name of the property to set
-     * @param mixed $value The value to set
-     * @throws ReflectionException
+     * Helper method to create a real TeleCashPayment instance for testing
      */
-    private function setProperty($object, $propertyName, $value): void
+    private function createRealTeleCashPayment(): TeleCashPayment
     {
-        $reflection = new ReflectionClass(get_class($object));
-        $property = $reflection->getProperty($propertyName);
-        $property->setValue($object, $value);
+        $payment = $this->getMockBuilder(TeleCashPayment::class)
+            ->setConstructorArgs([
+                $this->validatorMock,
+                $this->connectionMock,
+                false
+            ])
+            ->onlyMethods([
+                'init',
+                'save',
+                'isAdmin',
+                'getFieldNames'
+            ])
+            ->getMock();
+
+        $payment->method('init')
+            ->willReturn(true);
+
+        // Mock isAdmin method
+        $payment->method('isAdmin')
+            ->willReturn(false);
+
+        // Mock save method from parent
+        $payment->method('save')
+            ->willReturn(true);
+
+        // Mock getFieldNames if needed
+        $payment->method('getFieldNames')
+            ->willReturn(['oxid', 'oxpaymentid', 'telecashident', 'capturetype']);
+
+        return $payment;
     }
 
     /**
@@ -156,5 +159,173 @@ class TeleCashPaymentTest extends TestCase
         // Call the methods and assert the result
         $this->teleCashPayment->setTeleCashCaptureType($validCaptureType);
         $this->assertEquals($validCaptureType, $this->teleCashPayment->getTeleCashCaptureType());
+    }
+
+    /**
+     * Test getting possible TeleCash Idents
+     */
+    public function testGetPossibleTeleCashIdents(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $possibleIdents = $teleCashPaymentReal->getPossibleTeleCashIdents();
+
+        $this->assertEquals(Module::TELECASH_PAYMENT_IDENTS, $possibleIdents);
+        $this->assertIsArray($possibleIdents);
+        $this->assertNotEmpty($possibleIdents);
+    }
+
+    /**
+     * Test validating TeleCash Ident with valid input
+     */
+    public function testValidTeleCashIdentWithValidInput(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $validIdent = Module::TELECASH_PAYMENT_IDENTS[0];
+
+        $result = $teleCashPaymentReal->validTeleCashIdent($validIdent);
+
+        $this->assertEquals($validIdent, $result);
+    }
+
+    /**
+     * Test validating TeleCash Ident with invalid input
+     */
+    public function testValidTeleCashIdentWithInvalidInput(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $invalidIdent = 'invalid_ident';
+
+        $result = $teleCashPaymentReal->validTeleCashIdent($invalidIdent);
+
+        $this->assertEquals(Module::TELECASH_PAYMENT_IDENT_DEFAULT, $result);
+    }
+
+    /**
+     * Test getting possible TeleCash Capture Types
+     */
+    public function testGetPossibleTeleCashCaptureTypes(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $validIdent = Module::TELECASH_PAYMENT_IDENTS[0];
+
+        $captureTypes = $teleCashPaymentReal->getPossibleTeleCashCaptureTypes($validIdent);
+
+        $this->assertEquals(Module::TELECASH_CAPTURE_TYPES[$validIdent], $captureTypes);
+        $this->assertIsArray($captureTypes);
+        $this->assertNotEmpty($captureTypes);
+    }
+
+    /**
+     * Test validating TeleCash Capture Type with valid input
+     */
+    public function testValidTeleCashCaptureTypeWithValidInput(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $validIdent = Module::TELECASH_PAYMENT_IDENTS[0];
+        $validCaptureType = Module::TELECASH_CAPTURE_TYPES[$validIdent][0];
+
+        $result = $teleCashPaymentReal->validTeleCashCaptureType($validCaptureType, $validIdent);
+
+        $this->assertEquals($validCaptureType, $result);
+    }
+
+    /**
+     * Test validating TeleCash Capture Type with invalid input
+     */
+    public function testValidTeleCashCaptureTypeWithInvalidInput(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+        $validIdent = Module::TELECASH_PAYMENT_IDENTS[0];
+        $invalidCaptureType = 'invalid_capture_type';
+
+        $result = $teleCashPaymentReal->validTeleCashCaptureType($invalidCaptureType, $validIdent);
+
+        $this->assertEquals(Module::TELECASH_CAPTURE_TYPE_DIRECT, $result);
+    }
+
+    /**
+     * Test loading TeleCash Payment by PaymentId
+     */
+    public function testLoadByPaymentId(): void
+    {
+        $paymentId = 'test_payment_id';
+        $mockData = ['oxid' => 'test_oxid', 'field1' => 'value1'];
+        $shopId = 1;
+        $viewName = 'test_view';
+
+        // Configure mocks
+        $this->teleCashPayment->method('getViewName')->willReturn($viewName);
+        $this->teleCashPayment->method('getShopId')->willReturn($shopId);
+        $this->teleCashPayment->method('buildSelectString')
+            ->willReturn('SELECT * FROM test_table');
+        $this->teleCashPayment->method('assign')
+            ->willReturn(true);
+        $this->teleCashPayment->method('addField')
+            ->willReturn(true);
+        $this->teleCashPayment->method('isLoaded')
+            ->willReturn(true);
+
+        // Configure connection mock to return test data
+        $this->connectionMock->expects($this->once())
+            ->method('fetchAssociative')
+            ->willReturn($mockData);
+
+        $result = $this->teleCashPayment->loadByPaymentId($paymentId);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test loading TeleCash Payment by PaymentId with invalid data
+     */
+    public function testLoadByPaymentIdWithInvalidData(): void
+    {
+        $paymentId = 'invalid_payment_id';
+        $shopId = 1;
+        $viewName = 'test_view';
+
+        // Configure mocks
+        $this->teleCashPayment->method('getViewName')->willReturn($viewName);
+        $this->teleCashPayment->method('getShopId')->willReturn($shopId);
+        $this->teleCashPayment->method('buildSelectString')
+            ->willReturn('SELECT * FROM test_table');
+        $this->teleCashPayment->method('isLoaded')
+            ->willReturn(false);
+
+        // Configure connection mock to return false
+        $this->connectionMock->expects($this->once())
+            ->method('fetchAssociative')
+            ->willReturn(false);
+
+        $result = $this->teleCashPayment->loadByPaymentId($paymentId);
+
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test saving payment with validator
+     */
+    public function testSave(): void
+    {
+        $teleCashPaymentReal = $this->createRealTeleCashPayment();
+
+        $testPaymentId = 'test_payment';
+        $testIdent = Module::TELECASH_PAYMENT_IDENT_DEFAULT;
+        $testCaptureType = Module::TELECASH_CAPTURE_TYPE_DIRECT;
+
+        // Set test values directly
+        $teleCashPaymentReal->setTestValues(
+            $testPaymentId,
+            $testIdent,
+            $testCaptureType
+        );
+
+        // Configure validator mock to return true (payment exists)
+        $this->validatorMock->expects($this->once())
+            ->method('checkIfPaymentExists')
+            ->with($testPaymentId, $testIdent, $testCaptureType)
+            ->willReturn(true);
+
+        $this->assertTrue($teleCashPaymentReal->validateBeforeSave(), 'Validation before save should pass');
     }
 }
