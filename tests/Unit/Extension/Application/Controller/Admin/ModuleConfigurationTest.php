d7a6982bbb8074c91d7ae052a8f45a781b0f48c4
Lars Stegelitz <lars.stegelitz@oxid-esales.com>
2024-10-29 19:18:53 +0100

    removed Prophecy Test from ModuleConfigurationTest.php

--- tests/Unit/Extension/Application/Controller/Admin/ModuleConfigurationTest.php
+++ tests/Unit/Extension/Application/Controller/Admin/ModuleConfigurationTest.php
@@ -7,26 +7,19 @@ use OxidSolutionCatalysts\TeleCash\Core\Service\RegistryService;
 use OxidSolutionCatalysts\TeleCash\Settings\Service\ModuleFileSettingsService;
 use OxidSolutionCatalysts\TeleCash\Settings\Service\ModuleFileSettingsServiceInterface;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ModuleConfigurationTest extends TestCase
 {
-    use ProphecyTrait;
-
     protected $moduleConfiguration;
     protected $registryService;
     protected $fileSettingsService;
 
     protected function setUp(): void
     {
-        $this->moduleConfiguration = $this->prophesize(ModuleConfiguration::class);
-        $this->registryService = $this->prophesize(RegistryService::class);
-        $this->fileSettingsService = $this->prophesize(ModuleFileSettingsServiceInterface::class);
-
-        //$this->moduleConfiguration->reveal()->registryService = $this->registryService->reveal();
-        //$this->moduleConfiguration->reveal()->fileSettingsService = $this->fileSettingsService->reveal();
+        $this->moduleConfiguration = $this->createMock(ModuleConfiguration::class);
+        $this->registryService = $this->createMock(RegistryService::class);
+        $this->fileSettingsService = $this->createMock(ModuleFileSettingsServiceInterface::class);
     }
 
     public function testRender()
