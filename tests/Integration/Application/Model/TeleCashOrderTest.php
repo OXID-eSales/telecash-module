31d037cb5319b99846b3da0560ac2963e63d0bfc
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-11-23 14:52:37 +0100

    add CopyRight

--- tests/Integration/Application/Model/TeleCashOrderTest.php
+++ tests/Integration/Application/Model/TeleCashOrderTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
 namespace OxidSolutionCatalysts\TeleCash\Tests\Integration\Application\Model;
 
 use OxidSolutionCatalysts\TeleCash\Application\Model\TeleCashOrder;
--- tests/Unit/Application/Model/TeleCashOrderTest.php
+++ tests/Unit/Application/Model/TeleCashOrderTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
 namespace OxidSolutionCatalysts\TeleCash\Tests\Unit\Application\Model;
 
 use OxidSolutionCatalysts\TeleCash\Tests\Unit\Application\Model\TestClasses\TeleCashOrderTestClass;
