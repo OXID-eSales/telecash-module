94e61e9c5d7d09eb549da25d382b942c5497c7c2
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-11-26 20:22:15 +0100

    fill new Controller OrderTeleCash

--- src/Application/Controller/Admin/OrderTeleCash.php
+++ src/Application/Controller/Admin/OrderTeleCash.php
@@ -9,23 +9,129 @@ declare(strict_types=1);
 
 namespace OxidSolutionCatalysts\TeleCash\Application\Controller\Admin;
 
+use DateTime;
 use OxidEsales\Eshop\Application\Controller\Admin\AdminController;
+use OxidSolutionCatalysts\TeleCash\Application\Model\TeleCashOrder;
+use OxidSolutionCatalysts\TeleCash\Application\Model\TeleCashPayment;
 use OxidSolutionCatalysts\TeleCash\Core\Module;
+use OxidSolutionCatalysts\TeleCash\Core\Service\RegistryService;
+use OxidSolutionCatalysts\TeleCash\Exception\TeleCashException;
+use OxidSolutionCatalysts\TeleCash\Extension\Application\Model\Order;
+use OxidSolutionCatalysts\TeleCash\Extension\Application\Model\Payment;
+use OxidSolutionCatalysts\TeleCash\Traits\ModelGetter;
+use OxidSolutionCatalysts\TeleCash\Traits\ServiceContainer;
 
 class OrderTeleCash extends AdminController
 {
+    use ModelGetter;
+    use ServiceContainer;
+
     protected $_sThisTemplate = '@' . Module::MODULE_ID . '/admin/order_telecash';
 
-    /** @inheritdoc */
+    protected RegistryService $registryService;
+
+    /**
+     * Constructor for Order.
+     *
+     * Initializes a new instance of the OrderTeleCash class. This constructor can be used
+     * in both production and test environments due to its flexible parameter configuration.
+     *
+     * @param bool $initParent  Whether to initialize the parent BaseModel.
+     *                          Set to false in test environment to avoid
+     *                          OXID framework dependencies. Default is true.
+     */
+    public function __construct(
+        bool $initParent = true
+    ) {
+        if ($initParent) {
+            parent::__construct();
+        }
+
+        $this->setContainer($this->getContainer());
+        $this->registryService = $this->getRequiredService(
+            RegistryService::class,
+            'RegistryService'
+        );
+    }
+
+    /** @inheritdoc
+     * @throws TeleCashException
+     */
     public function render()
     {
         parent::render();
 
-        $oxId = $this->getEditObjectId();
-        if ($oxId) {
-            $this->addTplParam('oxid', $oxId);
-        }
+        $this->addTeleCashToTemplate();
 
-        return "order_downloads";
+        return $this->getTemplateName();
+    }
+
+
+    /**
+     * @throws TeleCashException
+     */
+    private function addTeleCashToTemplate(): void
+    {
+        $oxId = $this->getEditObjectId();
+
+        /** @var Order $order */
+        $order = $this->getOxidOrderModel();
+        $order->load($oxId);
+        $isNettoMode = $order->isNettoMode();
+
+        $isTeleCashOrder = $order->isTeleCashOrder();
+        $teleCashOrder = $order->getTeleCashOrder();
+
+        $paymentId = $order->getFieldStringData('oxpaymenttype');
+        $payment = $this->getOxidPaymentModel();
+
+        $payment->load($paymentId);
+        /** @var Payment $payment */
+        $teleCashPayment = $payment->getTeleCashPayment();
+
+        // these variables are needed in any case
+        $this->addTplParam('oxid', $oxId);
+        $this->addTplParam('teleCashModuleId', Module::MODULE_ID);
+        $this->addTplParam('isTeleCashOrder', $isTeleCashOrder);
+
+        if ($isTeleCashOrder && $teleCashOrder) {
+            $this->addTeleCashOrderToTemplate(
+                $teleCashOrder,
+                $isNettoMode,
+                $teleCashPayment
+            );
+        }
+    }
+
+    /**
+     * @throws TeleCashException
+     */
+    private function addTeleCashOrderToTemplate(
+        TeleCashOrder $teleCashOrder,
+        bool $isNettoMode,
+        ?TeleCashPayment $teleCashPayment
+    ): void {
+        $chargeTotal = $teleCashOrder->getChargeTotal();
+        $currency = $teleCashOrder->getCurrency();
+        $priceChargeTotal = $this->getPriceObj($chargeTotal, $isNettoMode);
+        $currencyObj = $this->registryService->getConfig()->getCurrencyObject($currency);
+
+        $dateTime = $teleCashOrder->getTxnDateTime();
+        $txnDateTime = $dateTime ? $dateTime->format('d.m.Y H:i:s') : '';
+
+        $this->addTplParam('priceChargeTotal', $priceChargeTotal);
+        $this->addTplParam('currencyObj', $currencyObj);
+        $this->addTplParam('paymentMethod', $teleCashOrder->getPaymentMethod());
+        $this->addTplParam('status', $teleCashOrder->getStatus());
+        $this->addTplParam('responseCode', $teleCashOrder->getProcessorResponseCode());
+        $this->addTplParam('txnDateTime', $txnDateTime);
+        $this->addTplParam('txnType', $teleCashOrder->getTxnType());
+        $this->addTplParam('ipgTransactionId', $teleCashOrder->getIpgTransactionId());
+        $this->addTplParam('endpointTransactionId', $teleCashOrder->getEndpointTransactionId());
+        $this->addTplParam('terminalId', $teleCashOrder->getTerminalId());
+
+        if ($teleCashPayment) {
+            $this->addTplParam('captureType', $teleCashPayment->getTeleCashCaptureType());
+        }
     }
 }
