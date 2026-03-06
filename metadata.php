0cd13d9e9495fbb515abd56018009a461ddb7a27
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-03-06 16:09:38 +0100

    release v1.0.0-rc.5

--- metadata.php
+++ metadata.php
@@ -61,7 +61,7 @@ $aModule = [
         'en' => 'This module provides the integration of the payment provider Telecash.',
     ],
     'thumbnail'   => 'pictures/logo.png',
-    'version'     => '1.0.0-rc.4',
+    'version'     => '1.0.0-rc.5',
     'author'      => 'OXID eSales AG',
     'url'         => '',
     'email'       => '',
